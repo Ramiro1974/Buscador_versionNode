f679ebead1256d7787dea04027737c16b1da68ef
desarrolloWeb_buscador_ramiro_reyes <39528343+Ramiro1974@users.noreply.github.com>
2022-12-08 06:51:17 -0500

    llenado de options de ciudad y tipo

--- public/App_.js
+++ public/App_.js
@@ -3,6 +3,7 @@ console.log('estoy funcionando');
 //variables 
 const xhttp = new XMLHttpRequest();
 let res;
+let ciudadS;
 let Bciudad;
 let Nciudad = [];
 let Ntipo = [];
@@ -87,70 +88,107 @@ function busquedaCiudadesResultado() {
 xhttp.open('GET', 'data.json', true);
 xhttp.send();
 xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        datos = JSON.parse(this.responseText);
-        document.querySelector('#buscar').addEventListener('click', traerdatos);
+        if (this.readyState == 4 && this.status == 200) {
+            datos = JSON.parse(this.responseText);
+            document.querySelector('#buscar').addEventListener('click', traerdatos);
 
-        function traerdatos() {
-            console.log('dentro de la función');
-            // con esto se puede acceder a acada uno de os metodos
+            function traerdatos() {
+                console.log('dentro de la función');
+                // con esto se puede acceder a acada uno de os metodos
 
-            xhttp.open('GET', 'data.json', true);
-            xhttp.send();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    let datos = JSON.parse(this.responseText);
-                    //console.log(datos);
+                xhttp.open('GET', 'data.json', true);
+                xhttp.send();
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        let datos = JSON.parse(this.responseText);
+                        //console.log(datos);
 
-                    busquedaCiudadesResultado();
-                    busquedaCiudadesResultadoCyT();
+                        busquedaCiudadesResultado();
+                        busquedaCiudadesResultadoCyT();
 
-                    //console.log(Nciudad);
+                        //console.log(Nciudad);
 
-                    //sacar ciudades repetidas
+                        ciudadSelect();
 
-                    Nciudad.forEach(function(item) {
-                        if (!uniqueCiudad.includes(item)) {
-                            uniqueCiudad.push(item);
-                        }
-                    });
-                    console.log(uniqueCiudad);
+                        //sacar ciudades repetidas
 
-                    //trae la ciudad seleccionada;
+                        Nciudad.forEach(function(item) {
+                            if (!uniqueCiudad.includes(item)) {
+                                uniqueCiudad.push(item);
+                            }
+                        });
+                        console.log(uniqueCiudad);
 
-                    let resultado = "New York";
-                    const Ciudades = datos.filter(function(c) {
-                        return (c.Ciudad === resultado);
-                    });
-                    console.log(Ciudades);
+                        //trae la ciudad seleccionada;
+
+                        let resultado = "New York";
+                        const Ciudades = datos.filter(function(c) {
+                            return (c.Ciudad === resultado);
+                        });
+                        console.log(Ciudades);
 
 
-                    //console.log(Ntipo);
+                        //console.log(Ntipo);
 
-                    //sacar ciudades Tipos
+                        //sacar ciudades Tipos
 
-                    Ntipo.forEach(function(item) {
-                        if (!uniqueTipo.includes(item)) {
-                            uniqueTipo.push(item);
-                        }
-                    });
-                    console.log(uniqueTipo);
+                        Ntipo.forEach(function(item) {
+                            if (!uniqueTipo.includes(item)) {
+                                uniqueTipo.push(item);
+                            }
+                        });
+                        console.log(uniqueTipo);
 
-                    //trae la ciudad seleccionada;
+                        //trae la Tipos seleccionada;
 
-                    let resultado2 = "Casa de Campo";
-                    const Tipo = datos.filter(function(c) {
-                        return (c.Tipo === resultado);
-                    });
-                    console.log(Tipo);
+                        let resultado2 = "Casa de Campo";
+                        const Tipos = datos.filter(function(c) {
+                            return (c.Tipo === resultado2);
+                        });
+                        console.log(Tipos);
+
+
+
+                    }
 
 
 
                 }
 
-
-
             }
+
+
         }
+
     }
+    //window.ciudadSelect();
+
+function ciudadSelect() {
+    $(document).ready(function() {
+        $('select').material_select();
+    });
+    console.log("en la funcion select");
+    let ciudadS = document.querySelector('#ciudad');
+
+    uniqueCiudad.forEach(function(numero) {
+        console.log(numero);
+        ciudadS.innerHTML += `
+           
+                <option value = "${numero}" >${numero}</option>
+           
+         `
+    });
+
+    console.log("en la funcion select");
+    let tipoS = document.querySelector('#tipo');
+
+    uniqueTipo.forEach(function(numero) {
+        console.log(numero);
+        tipoS.innerHTML += `
+           
+                <option value = "${numero}" >${numero}</option>
+           
+         `
+    });
+
 }
