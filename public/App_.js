250ca4d86d5265e484942360d1f425a097eb5925
desarrolloWeb_buscador_ramiro_reyes <39528343+Ramiro1974@users.noreply.github.com>
2022-12-08 20:23:57 -0500

    busqueda por tipos y ciudades y sin

--- public/App_.js
+++ public/App_.js
@@ -1,44 +1,93 @@
-//alert("estoy funcionando");
-console.log('estoy funcionando');
-//variables 
-const xhttp = new XMLHttpRequest();
-let res;
-let ciudadS;
-let Bciudad;
 let Nciudad = [];
 let Ntipo = [];
 let uniqueCiudad = [];
 let uniqueTipo = [];
 let datos;
 
-//inicializa select del documento HMTL
-/*
-$(document).ready(function(){
-    $('select').formSelect();
-  });
-*/
+document.addEventListener('click', traerdatos());
+
+function traerdatos() {
+    console.log('dentro de la funcion');
+
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', 'data.json', true);
+
+    xhttp.send();
+
+    xhttp.onload = function() {
+
+        if (this.readyState == 4 && this.status == 200) {
+            //console.log(this.responseText);
+            datos = JSON.parse(this.responseText);
+            //console.log(datos);
+
+            //se trae las ciudades y tipos
+
+            for (let contador of datos) {
+                Nciudad.push(contador.Ciudad);
+                Ntipo.push(contador.Tipo);
+            }
+
+            //sacar ciudades repetidas
+
+            Nciudad.forEach(function(item) {
+                if (!uniqueCiudad.includes(item)) {
+                    uniqueCiudad.push(item);
+                }
+            });
+            console.log(uniqueCiudad);
+
+            //sacar ciudades Tipos
+
+            Ntipo.forEach(function(item) {
+                if (!uniqueTipo.includes(item)) {
+                    uniqueTipo.push(item);
+                }
+            });
+            console.log(uniqueTipo);
+
+            $(document).ready(function() {
+                $('select').material_select();
+            });
+            console.log("en la funcion select");
+            let ciudadS = document.querySelector('#ciudad');
+
+            uniqueCiudad.forEach(function(numero) {
+                console.log(numero);
+                ciudadS.innerHTML += `
+                   
+                        <option value = "${numero}" >${numero}</option>
+                   
+                 `
+            });
+
+            console.log("en la funcion select");
+            let tipoS = document.querySelector('#tipo');
+
+            uniqueTipo.forEach(function(numero) {
+                console.log(numero);
+                tipoS.innerHTML += `
+                   
+                        <option value = "${numero}" >${numero}</option>
+                   
+                 `
+            });
 
 
-//se trae las ciudades y tipos
-function busquedaCiudadesResultadoCyT() {
-    for (let contador of datos) {
-        Nciudad.push(contador.Ciudad);
-        Ntipo.push(contador.Tipo);
+        }
+
     }
 }
-// trae los elementos al HTML y los hace visibles
+//llena el resultado de la busqueda
 
-
-function busquedaCiudadesResultado() {
+function busquedaCiudadesResultado(busqueda) {
     res = document.querySelector('#res');
 
     res.innerHTML = ''; //aqui se esta limpiado todo
-    for (let contador of datos) {
-        Nciudad.push(contador.Ciudad);
-        Ntipo.push(contador.Tipo);
-    }
 
-    for (let contador of datos) {
+
+    for (let contador of busqueda) {
         res.innerHTML += `
 <div class="card horizontal">
         <div class="card-image">
@@ -77,118 +126,68 @@ function busquedaCiudadesResultado() {
     </div>
 </div>`;
 
-
-
     }
 
 }
 
 
-//se trae los datos
-xhttp.open('GET', 'data.json', true);
-xhttp.send();
-xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            datos = JSON.parse(this.responseText);
-            document.querySelector('#buscar').addEventListener('click', traerdatos);
+document.querySelector('#buscar').addEventListener('click', tarerCiudades);
 
-            function traerdatos() {
-                console.log('dentro de la función');
-                // con esto se puede acceder a acada uno de os metodos
+function tarerCiudades() {
+    let ciudadesSelect = document.getElementById('ciudad');
+    let ciudadSelect = ciudadesSelect.value;
+    //alert(ciudadSelect);
 
-                xhttp.open('GET', 'data.json', true);
-                xhttp.send();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        let datos = JSON.parse(this.responseText);
-                        //console.log(datos);
+    let tiposSelect = document.getElementById('tipo');
+    let tipoSelect = tiposSelect.value;
+    //alert(tipoSelect + ' y ' + ciudadSelect);
 
-                        busquedaCiudadesResultado();
-                        busquedaCiudadesResultadoCyT();
+    //busqueda por ciudades
 
-                        //console.log(Nciudad);
-
-                        ciudadSelect();
-
-                        //sacar ciudades repetidas
-
-                        Nciudad.forEach(function(item) {
-                            if (!uniqueCiudad.includes(item)) {
-                                uniqueCiudad.push(item);
-                            }
-                        });
-                        console.log(uniqueCiudad);
-
-                        //trae la ciudad seleccionada;
-
-                        let resultado = "New York";
-                        const Ciudades = datos.filter(function(c) {
-                            return (c.Ciudad === resultado);
-                        });
-                        console.log(Ciudades);
-
-
-                        //console.log(Ntipo);
-
-                        //sacar ciudades Tipos
-
-                        Ntipo.forEach(function(item) {
-                            if (!uniqueTipo.includes(item)) {
-                                uniqueTipo.push(item);
-                            }
-                        });
-                        console.log(uniqueTipo);
-
-                        //trae la Tipos seleccionada;
-
-                        let resultado2 = "Casa de Campo";
-                        const Tipos = datos.filter(function(c) {
-                            return (c.Tipo === resultado2);
-                        });
-                        console.log(Tipos);
-
-
-
-                    }
-
-
-
-                }
-
-            }
-
-
-        }
-
+    if (ciudadSelect !== '' && tipoSelect === '') {
+        let resultado = ciudadSelect;
+        const Ciudades = datos.filter(function(c) {
+            return (c.Ciudad === resultado);
+        });
+        //console.log(Ciudades);
+        busquedaCiudadesResultado(Ciudades);
     }
-    //window.ciudadSelect();
 
-function ciudadSelect() {
-    $(document).ready(function() {
-        $('select').material_select();
-    });
-    console.log("en la funcion select");
-    let ciudadS = document.querySelector('#ciudad');
+    //busqueda por tipo
 
-    uniqueCiudad.forEach(function(numero) {
-        console.log(numero);
-        ciudadS.innerHTML += `
-           
-                <option value = "${numero}" >${numero}</option>
-           
-         `
-    });
+    if (tipoSelect !== '' && ciudadSelect === '') {
+        let resultado2 = tipoSelect;
+        const Tipos = datos.filter(function(c) {
+            return (c.Tipo === resultado2);
+        });
+        console.log(Tipos);
+        busquedaCiudadesResultado(Tipos);
+    }
 
-    console.log("en la funcion select");
-    let tipoS = document.querySelector('#tipo');
+    //busqueda por ciudades y tipos
 
-    uniqueTipo.forEach(function(numero) {
-        console.log(numero);
-        tipoS.innerHTML += `
-           
-                <option value = "${numero}" >${numero}</option>
-           
-         `
-    });
+    if (tipoSelect !== '' && ciudadSelect !== '') {
+        let resultado = ciudadSelect;
+        const Ciudades = datos.filter(function(c) {
+            return (c.Ciudad === resultado);
+        });
+
+
+        let resultado2 = tipoSelect;
+        const Tipos = Ciudades.filter(function(c) {
+            return (c.Tipo === resultado2);
+        });
+        console.log(Tipos);
+        busquedaCiudadesResultado(Tipos);
+    }
+
+    // trae todos las ciudades y tipos
+
+    if (tipoSelect === '' && ciudadSelect === '') {
+        busquedaCiudadesResultado(datos);
+    }
+
+
+
 
 }
